fe4b0baeef52661174db5be068db22bcffa41300
vanshavenger <vanshchopra101@gmail.com>
2024-09-24 04:10:08 +0530

    deep copy

--- prototype/DatabaseConnection.java
+++ prototype/DatabaseConnection.java
@@ -1,9 +1,13 @@
 package prototype;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseConnection {
 
     private String url;
     private String imp_data;
+    private List<String> domains = new ArrayList<>();
 
     public String getUrl() {
         return url;
@@ -18,14 +22,35 @@ public class DatabaseConnection {
     public void setImp_data(String imp_data) {
         this.imp_data = imp_data;
     }
+
+    public List<String> getDomains() {
+        return domains;
+    }
+
+    public void setDomains(List<String> domains) {
+        this.domains = domains;
+    }
     
+
     public void loadVeryImportantData() {
         this.imp_data = "Very important data";
+
+        domains.add("google.com");
+        domains.add("facebook.com");
+        domains.add("twitter.com");
+        domains.add("instagram.com");
+        // Simulate a long process
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String toString() {
-        return this.url + " " + this.imp_data;
+        return this.url + " " + this.imp_data + " " + this.domains;
     }
 
     @Override
@@ -33,6 +58,14 @@ public class DatabaseConnection {
         DatabaseConnection db = new DatabaseConnection();
         db.setUrl(this.url);
         db.setImp_data(this.imp_data);
+
+        List<String> domains = new ArrayList<>();
+
+        for (String domain : this.domains) {
+            domains.add(domain);
+        }
+
+        db.setDomains(domains);
         return db;
     }
     
--- prototype/Main.java
+++ prototype/Main.java
@@ -15,6 +15,18 @@ public class Main {
             DatabaseConnection db1 = db.clone();
             db1.setUrl("jdbc:mysql://localhost:3306/anotherdb");
             System.out.println(db1);
+
+            DatabaseConnection db2 = db.clone();
+
+            db.getDomains().remove(0);
+
+            System.out.println(db);
+            System.out.println(db2);
+            System.out.println(db1);
+
+
+
+
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
 
