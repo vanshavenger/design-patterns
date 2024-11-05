b4fc6ec2e9461d2ef9454c5ceaa798e93bae8863
vanshavenger <vanshchopra101@gmail.com>
2024-11-06 03:05:42 +0530

    Composite

---
+++ vansh-public/composite/SalesDepartment.java
@@ -0,0 +1,15 @@
+public class SalesDepartment implements Department {
+    private Integer id;
+    private String name;
+
+    public SalesDepartment(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public void printDepartmentName() {
+        System.out.println(getClass().getSimpleName());
+    }
+    
+}
