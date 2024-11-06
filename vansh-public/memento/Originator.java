e3a51e42145929699cd474bea30c09f24f616e5b
vanshavenger <vanshchopra101@gmail.com>
2024-11-06 15:29:20 +0530

    Memento

---
+++ vansh-public/memento/Originator.java
@@ -0,0 +1,21 @@
+public class Originator {
+
+    private String state;
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public Memento saveStateToMemento() {
+        return new Memento(this.state);
+    }
+
+    public void getStateFromMemento(Memento memento) {
+        this.state = memento.getState();
+    }
+    
+}
