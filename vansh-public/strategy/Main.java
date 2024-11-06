0b365e3d3c93a942e89a0feabe4fc1050314ba64
vanshavenger <vanshchopra101@gmail.com>
2024-11-06 19:35:42 +0530

    Strategy

---
+++ vansh-public/strategy/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        
+        public static void main(String[] args) {
+            Context context = new Context(new AddStrategy());
+            System.out.println("10 + 5 = " + context.executeStrategy(10, 5));
+            
+            context = new Context(new SubtractStrategy());
+            System.out.println("10 - 5 = " + context.executeStrategy(10, 5));
+            
+            context = new Context(new MultiplyStrategy());
+            System.out.println("10 * 5 = " + context.executeStrategy(10, 5));
+        }
+}
